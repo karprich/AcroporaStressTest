3ed17bc0635fdd42d1c5e624f48be34a8debeb20
Richard Karp <Rich@Richards-MacBook-Air.local>
2017-07-13 14:28:46 -0400

    Code and Scheudle changes

--- R/Scripts/GAMMs.R
+++ R/Scripts/GAMMs.R
@@ -16,6 +16,7 @@ addpoly <- function(x, y1, y2, col=alpha("lightgrey", 0.8), ...){
 
 # Load data
 load("Output/all.f.RData")
+all.f <- na.omit(all.f)
 all.f <- droplevels(all.f)
 
 # Create numeric vector for number of days since start date
@@ -29,7 +30,7 @@ mod <- gamm4(Y ~ Genotype + s(Days, k=4, by=Genotype), random=~(1|FragID), data=
 #summary(mod$mer)
 
 # Get fitted values
-newdata <- expand.grid(Days=seq_len(max(all.f$Days)),
+newdata <- expand.grid(Days=seq(0, max(all.f$Days)),
                        Genotype=levels(all.f$Genotype))
 newdata$fit <- predict(mod$gam, newdata)
 
@@ -42,6 +43,12 @@ sim <- Xp %*% t(Rbeta)
 # Extract 84% confidence intervals from simulation
 newdata$lci <- apply(sim, 1, quantile, 0.08)
 newdata$uci <- apply(sim, 1, quantile, 0.92)
+
+# Get rid of fitted values beyond day range for which we have data
+maxDays <- aggregate(all.f$Days, by=list(all.f$Genotype), FUN=max)
+for(i in 1:nrow(maxDays)) {
+  newdata <- newdata[!(newdata$Genotype==maxDays[i, "Group.1"] & newdata$Days>maxDays[i, "x"]),] 
+}
 
 # Plot results
 library(lattice)
@@ -77,18 +84,85 @@ plotfits(newdata, "1721")
 abline(v=17, col = "red")
 plotfits(newdata, "1722")
 abline(v=17, col = "red")
+plotfits(newdata, "1734")
+abline(v=17, col = "red")
 
-
+plotfits(newdata, c("1721", "1755", "1738"))
+plotfits(newdata, c("1755", "1721", "1738"))
 
 plotfits(newdata, c("1721", "1755", "1738", "1731", "1750"))
-plotfits(newdata, c("1735", "1758", "1755", "1736", "1738", "1731", "1733", "1721"))
+#Kelsey Genotypes
+plotfits(newdata, c("1725", "1739", "1736", "1737", "1745"))
+#Cooper
+plotfits(newdata, c("1730"))
+#Elkhorn
+plotfits(newdata, c("1745"))
+#Best
+plotfits(newdata, c("1735", "1729", "1751", "1759"))
+#Worst
+plotfits(newdata, c("1732", "1734", "1722", "1738","1730"))
+#Plot w/ Cooper, Kelsey, Elkhorn, Best, Worst
+png(filename = "Output/Figures/diseasegenotypes.png", width=9, height = 9, units = "in", res = 300)
+plotfits(newdata, c("1725", "1739", "1736", "1737", "1730", "1745", "1738", "1722","1735", "1729"))
+legend("bottomleft", inset=c(.1, 0), legend=c("Worst-1", "Kelsey-1", "Best-1", "Cooper", "Best-2", "Kelsey-2", "Kelsey-3", "Worst-2", "Kelsey-4", "Elkhorn"), bty="n",
+       y.intersp=0.3)
+dev.off()
+
+plotfits(newdata, c("1735", "1758", "1755", "1736", "1738", "1731", "1733", "1721", "1729"))
+plotfits(newdata, c("1721", "1722", "1723", "1725", "1726", "1727", "1728", "1729", "1730", "1731", "1732", "1733", "1734", "1735", "1736", "1737", "1738", "1739", "1741", "1743", "1745", "1747", "1748", "1750","1751", "1752", "1753", "1755", "1757", "1758", "1759"))
 abline(v=17, col = "red")
+abline(h= 0)
 plotfits(newdata, levels(all.f$Genotype))
 # Panel xyplot()
-png(filename = "Output/Figures/first12ipamscurve.png", width=7, height = 7, units = "in", res = 300)
+png(filename = "Output/Figures/first14ipamscurve.png", width=7, height = 7, units = "in", res = 300)
 xyplot(fit ~ Days | Genotype, data = newdata, type = "l")
 dev.off()
 
 ? xyplot
 # Plot Genotypes seperately with confident intervals and points
 
+#Normalize data
+for(geno in newdata$Genotype) {
+  sdata <- newdata[which(newdata$Genotype==geno),]
+  for(day in sdata$Days) {
+    sdata[which(sdata$Days ==day), "rfvfm"] <- sdata[which(sdata$Days==day), "fit"] / sdata[which(sdata$Days==0), "fit"]
+    sdata[which(sdata$Days ==day), "rlci"] <- sdata[which(sdata$Days==day), "lci"] / sdata[which(sdata$Days==0), "fit"]
+    sdata[which(sdata$Days ==day), "ruci"] <- sdata[which(sdata$Days==day), "uci"] / sdata[which(sdata$Days==0), "fit"]
+  }
+  newdata[which(newdata$Genotype==geno), "rfvfm"] <- sdata[, "rfvfm"]
+  newdata[which(newdata$Genotype==geno), "rlci"] <- sdata[, "rlci"]
+  newdata[which(newdata$Genotype==geno), "ruci"] <- sdata[, "ruci"]
+}
+
+plotfitsnormal <- function(data, geno) {
+  df <- droplevels(subset(data, Genotype %in% geno))
+  getColor <- colorRampPalette(brewer.pal(length(geno), "Dark2"))
+  colors <- getColor(length(geno))
+  dfsplit <- split(df, f=df$Genotype)
+  plot(NA, xlim=range(df$Days), ylim=range(df$rfvfm), xaxs="i", xlab = "Days", ylab = "Y")
+  for (i in 1:length(dfsplit)) {
+    with(dfsplit[[i]], {
+      lines(Days, rfvfm)
+      addpoly(Days, rlci, ruci, col=alpha(colors[i], 0.3))
+    })
+  }
+  legend("bottomleft", legend=levels(df$Genotype), pch=15, bty="n",
+         col=alpha(colors, 0.4), pt.cex=1.5,
+         y.intersp=0.3)
+}
+
+plotfitsnormal(newdata, c("1725", "1739", "1736", "1737", "1730", "1745", "1738", "1722","1735", "1729"))
+
+library(lattice)
+xyplot(rfvfm ~ Days, groups= Genotype, data=newdata, type="l")
+xyplot(rfvfm ~ Days | Genotype, data=newdata, type="l")
+plotfitsnormal(newdata, c("1722", "1732", "1738", "1735"))
+#Residuals OUtlier
+residualsModel <- residuals(mod$mer)
+sdmod <-sd(residuals(mod$mer))
+outliersMod <- residualsModel[residualsModel<= 2*sdmod]
+all.ff <- all.f[which(residualsModel < 2 * sdmod), ]
+mod.ff <- gamm4(Y ~ Genotype + s(Days, k=4, by=Genotype), random=~(1|FragID), data=all.ff)
+xyplot(Y~Date, groups=Genotype, data=all.ff)
+xyplot(Y ~ Days | Genotype, data=all.ff)
+xyplot(Y ~ Days | Genotype, data=all.f)
--- R/Scripts/ipamprocessing.R
+++ R/Scripts/ipamprocessing.R
@@ -14,6 +14,8 @@ genotypes9 <- read.csv("Data/19062017_9thIpam/Processed/rgenotypes.csv")
 genotypes10 <- read.csv("Data/21062017_10thIpam/Processed/rgenotypes.csv")
 genotypes11 <- read.csv("Data/23062017_11thIpam/Processed/rgenotypes.csv")
 genotypes12 <- read.csv("Data/26062017_12thIpam/Processed/rgenotypes.csv")
+genotypes13 <- read.csv("Data/28062017_13thIpam/Processed/rgenotypes.csv")
+genotypes14 <- read.csv("Data/30062017_14thIpam/Processed/rgenotypes.csv")
 
 #Import iPAM data
 devtools::source_url("https://raw.githubusercontent.com/jrcunning/IPAM2R/master/R/import_ipam.R")
@@ -37,16 +39,19 @@ ipam9_values <- import_ipam("Data/19062017_9thIpam/RawData",  info.pattern = NUL
 ipam10_values <- import_ipam("Data/21062017_10thIpam/RawData", info.pattern = NULL)
 ipam11_values <- import_ipam("Data/23062017_11thIpam/RawData", info.pattern = NULL)
 ipam12_values <- import_ipam("Data/26062017_12thIpam/RawData", info.pattern = NULL)
+ipam13_values <- import_ipam("Data/28062017_13thIpam/RawData", info.pattern = NULL)
+ipam14_values <- import_ipam("Data/30062017_14thIpam/RawData", info.pattern = NULL)
+
 
 # Check Photos and Files for Same AOI
-View(table(ipam12_values$file))
-View(table(genotypes12$Picture))
+View(table(ipam14_values$file))
+View(table(genotypes14$Picture))
 
 
 
 combine_ipam1 <- merge(genotypes1, ipam1_values, by = c("file", "AOI"))
 combine_ipam1$Date <- as.Date("2017-05-22")
-combine_ipam1$Tank <- NA
+combine_ipam1$Tank <- "Initial"
 combine_ipam2 <- merge(genotypes2, ipam2_values, by = c("file", "AOI"))
 combine_ipam2$Date <- as.Date("2017-05-25") # make new column with date field
 combine_ipam3 <- merge(genotypes3, ipam3_values, by = c("file", "AOI"))
@@ -73,6 +78,10 @@ combine_ipam11 <- merge(genotypes11, ipam11_values, by = c("file", "AOI"))
 combine_ipam11$Date <- as.Date("2017-06-23")
 combine_ipam12 <- merge(genotypes12, ipam12_values, by = c("file", "AOI"))
 combine_ipam12$Date <- as.Date("2017-06-26")
+combine_ipam13 <- merge(genotypes13, ipam13_values, by = c("file", "AOI"))
+combine_ipam13$Date <- as.Date("2017-06-28")
+combine_ipam14 <- merge(genotypes14, ipam14_values, by = c("file", "AOI"))
+combine_ipam14$Date <- as.Date("2017-06-30")
 
 #correction factor
 cor_combine_ipam5 <- combine_ipam5
@@ -92,12 +101,52 @@ cor_combine_ipam5$Y <- (cor_combine_ipam5$Fm-cor_combine_ipam5$F)/cor_combine_ip
 # reorder columns in ipam1
 combine_ipam1 <- combine_ipam1[,c(1:5,10,6:9)]
 # attach all rows together
-all <- rbind(combine_ipam1, combine_ipam2, combine_ipam3, cor_combine_ipam4, cor_combine_ipam5, combine_ipam6, combine_ipam7, combine_ipam8, combine_ipam9, combine_ipam10, combine_ipam11, combine_ipam12)
+all <- rbind(combine_ipam1, combine_ipam2, combine_ipam3, cor_combine_ipam4, cor_combine_ipam5, combine_ipam6, combine_ipam7, combine_ipam8, combine_ipam9, combine_ipam10, combine_ipam11, combine_ipam12, combine_ipam13, combine_ipam14)
 # remove rows with "BL" or "BACK" in them
 all.f <- subset(all, !all$Genotype %in% c("17BL", "BACK", "Blan"))
 # remove rows with 1732 and 1734 from 12th Ipam as they are dead
 all.f[which(all.f$Date=="2017-06-26" & all.f$Genotype=="1732"),"Y"]<- NA
 all.f[which(all.f$Date=="2017-06-26" & all.f$Genotype=="1734"),"Y"] <- NA
+#omit Data that were positively selected for:
+#1721 Ipam 13+14
+all.f[which(all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1721"),"Y"]<- NA
+#1722 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1722"),"Y"]<- NA
+#1727 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1727"),"Y"]<- NA
+#1728 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1728"),"Y"]<- NA
+#1731 Ipam 13+14
+all.f[which(all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1731"),"Y"]<- NA
+#1732 Ipam 12, 13+14 Previously deleted Ipam 12
+all.f[which(all.f$Date=="2017-06-26" || all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1732"),"Y"]<- NA
+#1734 Ipam 12, 13+14 Previously deleted Ipam 12
+all.f[which(all.f$Date=="2017-06-26" || all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1734"),"Y"]<- NA
+#1736 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1736"),"Y"]<- NA
+#1737 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1737"),"Y"]<- NA
+#1738 Ipam 13+14
+all.f[which(all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1738"),"Y"]<- NA
+#1739 Ipam 13+14
+all.f[which(all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1739"),"Y"]<- NA
+#1747 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1747"),"Y"]<- NA
+#1750 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1750"),"Y"]<- NA
+#1752 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1752"),"Y"]<- NA
+#1753 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1753"),"Y"]<- NA
+#1755 Ipam 13+14
+all.f[which(all.f$Date=="2017-06-28" || all.f$Date=="2017-06-30" & all.f$Genotype=="1755"),"Y"]<- NA
+#1757 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1757"),"Y"]<- NA
+#1758 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1758"),"Y"]<- NA
+#1759 Ipam 14
+all.f[which(all.f$Date=="2017-06-30" & all.f$Genotype=="1759"),"Y"]<- NA
+
 
 # Save data as .RData
 save(all.f, file="Output/all.f.RData")
@@ -107,14 +156,14 @@ save(all.f, file="Output/all.f.RData")
 #par(mfrow= c(1, 1))
 
 library(lattice)
-xyplot(Y ~ Date | Genotype, data=all.f, type=c("p", "r"), ylim=c(0.0, 0.7))
+xyplot(Y ~ Date | Genotype, data=all.f, type=c("p"), ylim=c(0.0, 0.7))
 
-boxplot(combine_ipam1$Y, combine_ipam2$Y, combine_ipam3$Y, cor_combine_ipam4$Y, cor_combine_ipam5$Y, combine_ipam6$Y, combine_ipam7$Y, combine_ipam8$Y, combine_ipam9$Y, combine_ipam10$Y, combine_ipam11$Y, combine_ipam12$Y, names = c("1st IPam", "2nd IPam", "3rd IPam", "4th IPam", "5th IPam", "6th IPam", "7th IPam", "8th IPam", "9th IPam", "10th IPam", "11th IPam", "12th IPam"), ylab = "Y", ylim = c(0.0, 0.7))
+boxplot(combine_ipam1$Y, combine_ipam2$Y, combine_ipam3$Y, cor_combine_ipam4$Y, cor_combine_ipam5$Y, combine_ipam6$Y, combine_ipam7$Y, combine_ipam8$Y, combine_ipam9$Y, combine_ipam10$Y, combine_ipam11$Y, combine_ipam12$Y, combine_ipam13$Y, combine_ipam14$Y, names = c("1st IPam", "2nd IPam", "3rd IPam", "4th IPam", "5th IPam", "6th IPam", "7th IPam", "8th IPam", "9th IPam", "10th IPam", "11th IPam", "12th IPam", "13th Ipam", "14th Ipam"), ylab = "Y", ylim = c(0.0, 0.7))
 #To look at numbers by genotype and decline
 #summ <- aggregate(all.f$FragID, by = list(all.f$Genotype, all.f$Date), FUN = length)
 #xyplot(x~ Group.2|Group.1, data = summ, type ="o", ylim = c(0, 17))
 
-xyplot(Y ~ factor(Tank) | factor(Genotype), data=combine_ipam11, type=c("p"), ylim = c(0.2, 0.7), xlab ="Tank")
+xyplot(Y ~ factor(Tank) | factor(Genotype), data=combine_ipam14, type=c("p"), ylim = c(0.0, 0.7), xlab ="Tank")
 #xyplot(Y ~ factor(Box) | factor(Genotype), data=combine_ipam6wbox, type=c("p"), ylim = c(0.3, 0.7))
 View(table(combine_ipam3$Tank))
 ? xyplot 
@@ -122,24 +171,30 @@ View(table(combine_ipam3$Tank))
 View(combine_ipam3)
 #Write figures
 #Write Genotype Plot
-png(filename = "Output/Figures/first12ipamsbygeno.png", width=7, height = 7, units = "in", res = 300)
-xyplot(Y ~ Date | Genotype, data=all.f, type=c("p", "r"), ylim=c(0.0, 0.7), main = "Ipam 12 by Genotype")
+png(filename = "Output/Figures/first14ipamsbygeno.png", width=7, height = 7, units = "in", res = 300)
+xyplot(Y ~ Date | Genotype, data=all.f, type=c("p"), ylim=c(0.0, 0.7), main = "Ipam 14 by Genotype")
 dev.off()
 #Write Boxplots
-png(filename = "Output/Figures/first12ipamsboxplot.png", width=3, height = 5, units = "in", res = 300)
-boxplot(combine_ipam1$Y, combine_ipam2$Y, combine_ipam3$Y, cor_combine_ipam4$Y, cor_combine_ipam5$Y, combine_ipam6$Y, combine_ipam7$Y, combine_ipam8$Y, combine_ipam9$Y, combine_ipam10$Y, combine_ipam11$Y, combine_ipam12$Y, names = c("1st IPam", "2nd IPam", "3rd IPam", "4th IPam", "5th IPam", "6th IPam", "7th IPam", "8th IPam", "9th IPam", "10th IPam", "11th IPam", "12th IPam"), ylab = "Y", ylim = c(0.0, 0.7))
+png(filename = "Output/Figures/first14ipamsboxplot.png", width=3, height = 5, units = "in", res = 300)
+boxplot(combine_ipam1$Y, combine_ipam2$Y, combine_ipam3$Y, cor_combine_ipam4$Y, cor_combine_ipam5$Y, combine_ipam6$Y, combine_ipam7$Y, combine_ipam8$Y, combine_ipam9$Y, combine_ipam10$Y, combine_ipam11$Y, combine_ipam12$Y, combine_ipam13$Y, combine_ipam14$Y, names = c("1st IPam", "2nd IPam", "3rd IPam", "4th IPam", "5th IPam", "6th IPam", "7th IPam", "8th IPam", "9th IPam", "10th IPam", "11th IPam", "12th IPam", "13th IPam", "14th IPam"), ylab = "Y", ylim = c(0.0, 0.7))
 dev.off()
 #Write Tanks plot
 png(filename = "Output/Figures/ipam9bytank.png", width=5, height = 5, units = "in", res = 300)
-xyplot(Y ~ factor(Tank) | factor(Genotype), data=combine_ipam8, type=c("p"), ylim = c(0.4, 0.7), xlab = "Tank", main = "Ipam 9 by Tank")
+xyplot(Y ~ factor(Tank) | factor(Genotype), data=combine_ipam14, type=c("p"), ylim = c(0.0, 0.7), xlab = "Tank", main = "Ipam 9 by Tank")
 dev.off()
 
 #mean Y Table
 y_meangeno_10 <-aggregate(combine_ipam10$Y, by = list(combine_ipam10$Genotype), FUN = mean)
 y_meangeno_11 <-aggregate(combine_ipam11$Y, by = list(combine_ipam11$Genotype), FUN = mean)
 y_meangeno_12 <-aggregate(combine_ipam12$Y, by = list(combine_ipam12$Genotype), FUN = mean)
+y_meangeno_13 <-aggregate(combine_ipam13$Y, by = list(combine_ipam13$Genotype), FUN = mean)
+y_meangeno_14 <-aggregate(combine_ipam14$Y, by = list(combine_ipam14$Genotype), FUN = mean)
 ipam_count <- table(combine_ipam12$Genotype)
-View(cbind(ipam_count, y_meangeno_10, y_meangeno_11, y_meangeno_12))
+ipam_count_13 <- table(combine_ipam13$Genotype)
+ipam_count_14 <- table(combine_ipam14$Genotype)
+View(aggregate(ipam_count_13, y_meangeno_13, FUN = paste))
+View(aggregate(ipam_count_14, y_meangeno_14, FUN = paste))
+View(cbind(ipam_count, y_meangeno_10, y_meangeno_11, y_meangeno_12, y_meangeno_13))
 write.csv(y_meangeno, "Output/8thIpamYmean.csv")
 
 #Number of Genotypes with x number of genotypes
@@ -177,9 +232,9 @@ library(multcompView)
 library(lsmeans)
 
 
-mod_Tank <- lm(Y ~ Tank, data= combine_ipam12)
-combine_ipam12$Genotype <- factor(combine_ipam12$Genotype)
-mod_Geno <- lm(Y ~ Genotype, data = combine_ipam12)
+mod_Tank <- lm(Y ~ Tank, data= combine_ipam13)
+combine_ipam13$Genotype <- factor(combine_ipam13$Genotype)
+mod_Geno <- lm(Y ~ Genotype, data = combine_ipam13)
 
 all.f$Date <- factor(all.f$Date)
 all.f$Genotype <- factor(all.f$Genotype)
@@ -191,6 +246,7 @@ mod_all <- lm(Y ~ Date * Genotype, data=all.f)
 anova(mod_Geno)
 
 cld(lsmeans(mod_Tank_all, "Tank"))
+cld(lsmeans(mod_Tank, "Tank"))
 cld(lsmeans(mod_Geno, "Genotype"))
 cld(lsmeans(mod_Date, "Date"))
 cld(lsmeans(mod_all, specs = c("Date", "Genotype")))
